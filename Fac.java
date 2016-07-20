26981048f67fc20d4dcd810d9277bc6e2b3bbd3d
rkgowripriya <gowripriyasurya@gmail.com>
2016-07-20 17:12:25 +0530

    Update and rename Facto.java to Fac.java

---
+++ Fac.java
@@ -0,0 +1,7 @@
+class Fac{
+public static void main(String args[])
+{
+String s=args[0]; 	
+StringBuffer buf=new StringBuffer(s);
+String s1=buf.reverse().toString(); 	
+System.out.println(s1); 		 	} }
